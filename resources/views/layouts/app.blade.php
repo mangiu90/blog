b45d96356b7ba23d4c5127ec345eee5e532cd2d5
Manuel <giudicimanuel@gmail.com>
2020-12-14 14:37:02 -0300

    Creo menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,14 +20,8 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-dropdown')
+            @livewire('navigation')
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
 
             <!-- Page Content -->
             <main>
